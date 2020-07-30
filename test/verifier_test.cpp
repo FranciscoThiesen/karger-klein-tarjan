de2d386097f160fd5142ed3401377efe02cda541
FranciscoThiesen <franciscogthiesen@gmail.com>
2020-07-30 19:44:30 -0300

    Adding extra test for verification

--- test/verifier_test.cpp
+++ test/verifier_test.cpp
@@ -5,7 +5,7 @@
 #include "../graph_utils/graph_utils.hpp"
 #include "gtest/gtest.h"
 
-TEST(positive, random_graph)
+TEST(positive_verification, random_graph)
 {
     auto random_graph = build_random_connected_graph(100, 500);
     auto kruskal_mst = kruskal(random_graph, 100);
@@ -14,6 +14,20 @@ TEST(positive, random_graph)
     ASSERT_TRUE(verify_mst(random_graph, boruvka_mst, 100));
     auto prim_mst = prim(random_graph, 100);
     ASSERT_TRUE(verify_mst(random_graph, prim_mst, 100));
+}
+
+TEST(negative_verification, fixed_graph)
+{
+    vector< tuple<int, int, int> > simple_graph;
+    simple_graph.emplace_back(0, 1, 1);
+    simple_graph.emplace_back(1, 2, 1);
+    simple_graph.emplace_back(0, 2, 2);
+
+    vector< tuple<int, int, int> > non_minimum_spanning_tree;
+    non_minimum_spanning_tree.emplace_back(0, 2, 2);
+    non_minimum_spanning_tree.emplace_back(1, 2, 1);
+
+    ASSERT_FALSE(verify_mst(simple_graph, non_minimum_spanning_tree, 3));
 }
 
 TEST(stress_test, random_small_graph)
@@ -63,4 +77,5 @@ TEST(stress_test, random_large_graph)
         ASSERT_EQ(is_mst, verifier_result);
     }
 }
+
 
