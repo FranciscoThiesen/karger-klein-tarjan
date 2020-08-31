1545f99302a8feab3c25624be0e6f9e768dfee79
FranciscoThiesen <franciscogthiesen@gmail.com>
2020-08-31 11:45:49 -0300

    Adding function signature for KKT algorithm

--- karger/karger.cpp
+++ karger/karger.cpp
@@ -1,7 +1,31 @@
-#include <bits/stdc++.h>
+// Esboco inicial da implementação...
 
-using namespace std;
 
+// Essa função vai aplicar o passo Boruvka, descrito no paper do KKT. A ideia é diminuir 
+// a quantidade de vértices pela metade, ainda em tempo linear.
+
+// Se eu quiser retornar as arestas utilizadas, preciso pensar em manter para cada nó, qual super-nó ele pertence..
+// Mesmo que as extremidades das arestas mudem ao longo do tempo, posso manter um índice que permite que cada aresta
+// da MST seja mapeada para alguma das arestas originais.
+vector< tuple<int, int, int> > boruvka_step(const vector< tuple<int, int, int> >& G) {
+    // implementar!
+    // Aqui, preciso retornar o grafo resultante e também as arestas que foram utilizadas!
+}
+
+// Seleciona arestas de G com probabilidade = 1/2. Esse processo é descrito no paper do KKT,
+// onde essa etapa é chamada de Random Sampling.
+vector< tuple<int, int, int> > random_sampling(const vector< tuple<int, int, int> >& G) {
+    vector< tuple<int, int, int> > H;
+    for(const auto& E : G) {
+        int b = (rand() & 1);
+        if(b) H.push_back(E);
+    }
+    return H;
+}
+
+// Retorna o custo da minimum-spanning-tree, em tempo esperado linear.
+// Seguindo o processo descrito pelo paper KKT.
 vector< tuple<int, int, int> > kkt(const vector< tuple<int, int, int> >& G) {
-
+    vector< tuple<int, int, int> > resp;
+    if(G.empty()) return resp;
 }
--- verifier/verifier.hpp
+++ verifier/verifier.hpp
@@ -9,7 +9,7 @@ using namespace std;
 // Esse método busca utilizar o algoritmo O(n) de verificação descrito por
 // Tarjan et. al para verificar se uma determinada arvore geradora eh de fato
 // minima Essa função assume que está recebendo uma arvore geradora!
-bool verify_mst(const vector<tuple<int, int, int>> graph,
-				const vector<tuple<int, int, int>> spanning_tree, const int n);
+bool verify_mst(const vector<tuple<int, int, int>>& graph,
+				const vector<tuple<int, int, int>>& spanning_tree, const int n);
 
 #endif
